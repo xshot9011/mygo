af1a2f4ad0512780497f06f97d4f248548051648
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2022-04-16 23:19:10 +0700

    Feature function on log (#5)
    * (add): function to call with package
    
    * (fix): naming and space

--- logger/logger.go
+++ logger/logger.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"fmt"
 	"os"
 	"runtime"
 
@@ -26,21 +25,67 @@ func init() {
 	}
 	log.SetFormatter(customFormatter)
 	log.SetLevel(logLevel)
-
-	// The log level is the hierarchy.
-	// log.Trace("Successfully initial log Trace")
-	// log.Debug("Successfully initial log Debug")
-	// log.Info("Successfully initial log Info")
-	// log.Warn("Successfully initial log Warn")
-	// log.Error("Successfully initial log Error")
-	// log.Fatal("Bye.")         // Calls os.Exit(1) after logging
-	// log.Panic("I'm bailing.") // Calls panic() after logging
 }
 
-func Trace() string {
+func Trace(args ...interface{}) {
+	log.Trace(args...)
+}
+
+func Tracef(format string, args ...interface{}) {
+	log.Tracef(format, args...)
+}
+
+func Debug(args ...interface{}) {
+	log.Debug(args...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	log.Debugf(format, args...)
+}
+
+func Info(args ...interface{}) {
+	log.Info(args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+
+func Warn(args ...interface{}) {
+	log.Warn(args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	log.Warnf(format, args...)
+}
+
+func Error(args ...interface{}) {
+	log.Error(args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+}
+
+func Fatal(args ...interface{}) {
+	log.Fatal(args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	log.Fatalf(format, args...)
+}
+
+func Panic(args ...interface{}) {
+	log.Panic(args...)
+}
+func Panicf(format string, args ...interface{}) {
+	log.Panicf(format, args...)
+}
+
+func LogFunction() {
 	pc := make([]uintptr, 10)
 	runtime.Callers(2, pc)
 	f := runtime.FuncForPC(pc[0])
 	_, line := f.FileLine(pc[0])
-	return fmt.Sprintf("Invoke at %s.%d", f.Name(), line)
+	log.Tracef("Invoke at %s.%d", f.Name(), line)
 }
